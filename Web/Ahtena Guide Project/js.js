814315580ad8f13cd59e247c38e974e6812d6287
pionium <0pionium@gmail.com>
2014-02-19 20:40:08 +0900

    php 파일 추가 및 업데이트
    javascript 업데이트

--- Web/Ahtena Guide Project/js.js
+++ Web/Ahtena Guide Project/js.js
@@ -1,5 +1,16 @@
-var a = document.getElementById('sort').value;
+var sortOp = document.getElementById("sort");
+var sort_draw = document.getElementById("drawMain");
+	
+function sortChoose() {
+	if (sortOp.selectedIndex == 0) {
+		sort_draw.innerHTML = "";
+	} else {
+		//alert(sortOp.options[sortOp.selectedIndex].value);
+		//sort_draw.innerHTML = sortOp.options[sortOp.selectedIndex].text;
+		dataReq(sortOp.selectedIndex);
+	}
+}
 
-function	testFunc() {
-	alert(a);
+function dataReq(sOption) {
+	
 }
