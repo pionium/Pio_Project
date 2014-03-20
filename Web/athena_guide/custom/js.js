70e87ca2b04d5240596c47cf19f0734771ce6c8d
pionium <0pionium@gmail.com>
2014-03-20 22:57:28 +0900

    스크립트 로드 defer 속성값 변경 및 창 리사이즈 함수 추가
    defer 속성값을 추가하여야 올바르게 스크립트 를 로드 마지막에 로드하게 되며, 브라우저 사이즈 변경시 컨텐츠 사이즈 변경
    스크립트 함수 추가

--- Web/athena_guide/custom/js.js
+++ Web/athena_guide/custom/js.js
@@ -1,9 +1,25 @@
 ﻿//var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 //var h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
+window.onload = function() {
+	alert($("#main").css("margin-left"));
+	alert($("#main").css("padding-left"));
+}
+
+function basic_size() {
+	$("#wrap").width("1280px");
+	$("#main").width("1033px");
+}
+
+function re_size() {
+	var main_section = $("#main");
+	$("#warp").width(window.innerWidth - 20 + "px");
+	main_section.width(($("#wrap").innerWidth() - (main_section.css("margin-left") + main_section.css("padding-left") + main_section.css("border-width"))*2 - $("#right_aside").outerWidth(true)) + "px");
+}
+
 window.onresize = function() {
 	if(Number(window.innerWidth) < 1280)
-		$("#warp").width("1280px");
+		basic_size();
 	else
-		$("#warp").width(window.innerWidth-20 + "px");
-} 
+		re_size();
+}
