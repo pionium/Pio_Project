efa829ae6f1390d517a9630f7b5897e6c17adeaa
pionium <0pionium@gmail.com>
2014-03-20 17:19:33 +0900

    새 버전 Athena Guide 추가
    php 사용을 위한 새 버전 파일들 추가
    브라우저 사이즈 변경 시 엘리먼트 사이즈 변경

---
+++ Web/athena_guide/custom/js.js
@@ -0,0 +1,9 @@
+﻿//var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+//var h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+window.onresize = function() {
+	if(Number(window.innerWidth) < 1280)
+		$("#warp").width("1280px");
+	else
+		$("#warp").width(window.innerWidth-20 + "px");
+} 
