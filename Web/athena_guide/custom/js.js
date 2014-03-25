e99c121868ffcb31d9057ce910f10a45940184d0
pionium <0pionium@gmail.com>
2014-03-25 19:44:07 +0900

    창 사이즈 조절 적용
    초기 컨텐츠 사이즈 및 윈도우 사이즈 변경시 컨텐츠 조절 코드 적용

--- Web/athena_guide/custom/js.js
+++ Web/athena_guide/custom/js.js
@@ -1,25 +1,20 @@
 ﻿//var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 //var h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+//window.onload = window_change();
 
-window.onload = function() {
-	alert($("#main").css("margin-left"));
-	alert($("#main").css("padding-left"));
-}
-
-function basic_size() {
-	$("#wrap").width("1280px");
-	$("#main").width("1033px");
-}
+window.onload = function() {re_size();};
+window.onresize = function() {re_size();};
 
 function re_size() {
-	var main_section = $("#main");
-	$("#warp").width(window.innerWidth - 20 + "px");
-	main_section.width(($("#wrap").innerWidth() - (main_section.css("margin-left") + main_section.css("padding-left") + main_section.css("border-width"))*2 - $("#right_aside").outerWidth(true)) + "px");
-}
-
-window.onresize = function() {
-	if(Number(window.innerWidth) < 1280)
-		basic_size();
-	else
-		re_size();
+	if(Number(window.innerWidth) <= 815) {
+		$("#wrap").width("815px");
+		$("#main").width("568px");
+	} else {
+		$("#wrap").width(window.innerWidth - 20 + "px");
+		$("#main").width(($("#wrap").innerWidth() - (31*2) - 185) + "px");
+	}
+	//aside outer width is 185px
+	//main section outer width is 625px
+	//main section width is 563px
+	//main section margin + border + padding is 62px
 }
