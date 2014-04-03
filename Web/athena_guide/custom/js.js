0f3dfdedc10bb12f865f7b67057ed01274865060
pionium <0pionium@gmail.com>
2014-04-03 17:37:16 +0900

    data load 함수 추가 및 높이 조절 개선
    main 컨텐츠 높이 맞춤

--- Web/athena_guide/custom/js.js
+++ Web/athena_guide/custom/js.js
@@ -2,8 +2,8 @@
 //var h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 //window.onload = window_change();
 
-window.onload = function() {re_size();};
-window.onresize = function() {re_size();};
+window.onload = function() {re_size();monitor();};
+window.onresize = function() {re_size();monitor();};
 
 function re_size() {
 	if(Number(window.innerWidth) <= 815) {
@@ -17,15 +17,21 @@ function re_size() {
 	//main section outer width is 625px
 	//main section width is 563px
 	//main section margin + border + padding is 62px
-	
-	if(Number(window.innerHeight) <= 796) {
-		//$("#wrap").height("776px");
-		$("#main").height("600px");
+
+	if(Number(window.innerHeight) <= 510) {
+		$("#main").height("314px");
 	} else {
-		//$("#wrap").height(window.innerWidth - 20 + "px");
 		$("#main").height((Number(window.innerHeight) - 196) + "px");
 	}
 	//body header height is 42px
 	//body footer height is 57px
 	//body section outer height is 77px
+}
+
+function load_data(){
+	//if is there iframe element more than one,  refresh iframe content.
+}
+
+function monitor() {
+	$(".ad").first().text(window.innerWidth + " X " + window.innerHeight);
 }
